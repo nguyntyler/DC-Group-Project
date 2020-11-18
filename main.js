65e7d2a89f28a931e25e937568831aa1c72f2362
Art Lees <art.lees@dragonpeace.com>
2020-11-18 12:19:17 -0500

    201118 1st push

--- main.js
+++ main.js
@@ -13,22 +13,38 @@ button.addEventListener("click", () => {
 let removeUp = document.querySelector("#up");
 removeUp.addEventListener("click", () => {
   //   thumbsuprollout();
-  //   removeAvatar();
-});
+  updateEffects();
+  setTimeout(updateEffectsReset, 2000);
+  setTimeout(removeAvatar, 2050);
+  });
 
 let removeDown = document.querySelector("#down");
-// removeDown.addEventListener("click", removeAvatar);
+removeDown.addEventListener("click", () => {
+  updateEffects2();
+  setTimeout(updateEffectsReset, 2000);
+  setTimeout(removeAvatar, 2050);
+});
 
-removeUp.addEventListener("click", updateEffects);
+// removeUp.addEventListener("click", updateEffects);
 function updateEffects() {
+  // setTimeout(updateEffectsReset, 3000)
+  // (setTimeout(removeAvatar, 3010))
   //   document.documentElement.style.setProperty(`--transitiontime`, "1s");
   document.documentElement.style.setProperty(`--avatarx`, "-350vw");
   document.documentElement.style.setProperty(`--avatarslide`, "-900deg");
 }
 
-removeDown.addEventListener("click", updateEffects2);
+// removeDown.addEventListener("click", updateEffects2);
 function updateEffects2() {
   //   document.documentElement.style.setProperty(`--transitiontime`, "1s");
   document.documentElement.style.setProperty(`--avatary`, "200vh");
   document.documentElement.style.setProperty(`--avatarslide`, "-360deg");
 }
+
+export function updateEffectsReset() {
+  document.documentElement.style.setProperty(`--avatary`, "0px");
+  document.documentElement.style.setProperty(`--avatarx`, "0px");
+  document.documentElement.style.setProperty(`--avatarslide`, "0 deg");
+}
+
+
